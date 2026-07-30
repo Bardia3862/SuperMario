978d6a2515e8da628a6bd6786d339b7ba343eb04
Mohamad Bardia <Bardia3862@chmail.ir>
2026-07-30 10:40:37 +0330

    SuperMario

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-import pgzrun
 import random
+import pgzrun
 from pgzero.actor import Actor
 from pgzero.keyboard import keyboard
 
 
+def random_location(sprite):
+    sprite.x = random.randint(sprite.width//2,WIDTH-sprite.width//2 )
+    sprite.y = random.randint(sprite.height//2,HEIGHT-sprite.height//2)
 
 def draw():
-    screen.blit("background",(0,0))
+    screen.blit("background",(0, 0))
     mario.draw()
     luigi.draw()
+    coin.draw()
+    screen.draw.text(f"Mario Score: {mario.score}", (10,10), fontsize=30, color="yellow" )
+    screen.draw.text(f"Luigi Score: {luigi.score}", (1100,10), fontsize=30, color="yellow" )
 
 def update():
     # Mario section
@@ -22,30 +28,51 @@ def update():
         mario.y -= 5
     if keyboard.down:
         mario.y += 5
-    
+    if mario.colliderect(coin):
+        mario.score += 10
+        random_location(coin)
+
     # Luigi section
-    if keyboard.s:
+    if keyboard.n:
         luigi.x += 5
         luigi.image = "luigi_right"
-    if keyboard.a:
+    if keyboard.m:
+        luigi.x += 10
+        luigi.image = "luigi_right"
+    if keyboard.v:
         luigi.x -= 5
         luigi.image = "luigi_left"
-    if keyboard.w:
+    if keyboard.c:
+        luigi.x -= 10
+        luigi.image = "luigi_left"
+    if keyboard.g:
         luigi.y -= 5
-    if keyboard.z:
+    if keyboard.h:
+        luigi.y -= 5
+    if keyboard.y:
+        luigi.y -= 10
+    if keyboard.space:
         luigi.y += 5
+    if keyboard.b:
+        luigi.y += 10
+    if luigi.colliderect(coin):
+        luigi.score += 10
+        random_location(coin)
 
 
 WIDTH = 1280
 HEIGHT = 720
 
 mario = Actor('mario_right')
-mario.x = random.randint(0,WIDTH)
-mario.y = random.randint(0,HEIGHT)
+random_location(mario)
+mario.score = 0
+
 luigi = Actor('luigi_right')
-luigi.x = random.randint(0,WIDTH)
-luigi.y = random.randint(0,HEIGHT)
+random_location(luigi)
+luigi.score = 0
+
+coin = Actor('coin')
+random_location(coin)
 
 
 pgzrun.go()
-
