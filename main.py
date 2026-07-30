3674980bf6e719c56152aa68dd527ded768e9c2c
Mohamad Bardia <Bardia3862@chmail.ir>
2026-07-30 10:34:43 +0330

    SuperMario_02

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import pgzrun
+import random
+from pgzero.actor import Actor
+
 
 
 def draw():
-    pass
+    screen.blit("background",(0,0))
+    mario.draw()
+    luigi.draw()
 
 def update():
     pass
 
 
+WIDTH = 1280
+HEIGHT = 720
+
+mario = Actor('mario_right')
+mario.x = random.randint(0,WIDTH)
+mario.y = random.randint(0,HEIGHT)
+luigi = Actor('luigi_right')
+luigi.x = random.randint(0,WIDTH)
+luigi.y = random.randint(0,HEIGHT)
+
+
 pgzrun.go()
+
